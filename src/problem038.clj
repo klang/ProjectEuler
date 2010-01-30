63f7bf1441035cae2921490387950ed916253607
Karsten Lang <karstenlang@gmail.com>
2010-01-30 19:08:58 +0100

    not done

--- src/problem038.clj
+++ src/problem038.clj
@@ -2,3 +2,7 @@
 (use 'clojure.contrib.combinatorics)
 
 (count  (permutations '(1 2 3 4 5 6 7 8 9)))
+
+;; find a permutation, p that can be split in n pieces, where
+;; (= (reduce + (* n piece-n)) p)
+
