74f602e022a79cbd203c7715a1a884ac73873992
Karsten Lang <karstenlang@gmail.com>
2010-02-03 19:10:07 +0100

    not done .. not practical

---
+++ src/problem268.clj
@@ -0,0 +1,95 @@
+(load "tools")
+(use 'clojure.contrib.combinatorics)
+
+;; It can be verified that there are 23 positive integers less than 1000 that are divisible by at least four distinct primes less than 100.
+
+;;  Find how many positive integers less than 10^16 are divisible by at least four distinct primes less than 100.
+
+(defn distinct-factors [n]
+  (count (set (prime-factors n))))
+
+(defn distinct-factors-less-than-100 [n]
+  (count (filter #(< % 100) (set (prime-factors n)))))
+
+;; (reduce #(< 4 %) (map #(distinct-factors-less-than-100 %) (range 1 1001)))
+
+(defn problem268 [n]
+  (count (filter #(<= 4 % ) 
+		 (map #(distinct-factors-less-than-100 %) 
+		      (range 1 n)))))
+
+(deftest test-problem268
+  (is (= 23 (problem268 1000))))
+
+; (run-tests)
+;; (time (problem268 (expt 10 16)))
+;; runs too long
+
+(def my-primes (take-while #(<= % 100) primes))
+
+;; take 4 different primes from my-primes
+;; filter all the products that are less than 10^16
+(count (selections my-primes 4))
+;; user> (count (selections my-primes 4))
+;; 390625
+;; one prime is allowed to be present more than once ..
+
+;; only check even numbers .. 
+;; probably not a good idea, as we are not looking for primes, 
+;; but numbers that have a specific number of factors
+
+(defn problem268factors [n]
+  "returns false fast, if factors higher than 100 are found"
+  (loop [pfs []
+	 p (take-while #(<= % 100) primes)  ;; we only accept 'small' primes
+	 number n]
+    (if (or (= 0 (count p)) ;; there is no more primes to try with
+	    (= number 1))   ;; number is prime and has only 1 factor
+      false ;pfs            ;; this is not the number we are looking for
+      (let [f (first p)]
+	(if (zero? (rem number f))
+	  ;; prime f is a factor of number 
+	  ;; (f might still be a factor, but we do not care, use next p)
+	  ;; .. come to think about it, break out and return true if 
+	  ;; 3 different primefactors have already been found (making f the 4th)
+	  (if (<= 3 (count pfs))
+	    true ; (conj pfs f)
+	    (recur (conj pfs f) (rest p) (quot number f)))
+	  ;; try with the next prime
+	  (recur pfs (rest p) number))))
+    ))
+
+(deftest test-problem268factors
+  (is (or (= true (problem268factors (* 89 97 83 79 79 79)))
+	  (= [79 83 89 97] (problem268factors (* 89 97 83 79 79 79)))))
+  (is (or (= false (problem268factors (* 89 97 83 197)))
+	  (= [83 89 97] (problem268factors (* 89 97 83 197)))))
+  (is (or (= false (problem268factors 1))
+	  (= [] (problem268factors 1)))))
+
+;; (map #(factors %) (filter #(if (problem268factors %) %) (range 1 2000)))
+
+(defn problem268 [n]
+  (count (filter #(<= 4 % ) 
+		 (map #(distinct-factors-less-than-100 %) 
+		      (range 1 n)))))
+
+
+(defn problem268f [n]
+  (count (filter #(if (problem268factors %) %) (range 1 n))))
+
+;; user> (time (problem268f 1000))
+;; "Elapsed time: 107.056661 msecs"
+;; 23
+;; user> (time (problem268 1000))
+;; "Elapsed time: 49.863615 msecs"
+
+(defn problem268f [n]
+  (count (filter #(problem268factors %) (range 1 n))))
+;; user> (time (problem268f 1000))
+;; "Elapsed time: 99.271593 msecs"
+;; user> (time (problem268f 10000))
+;; "Elapsed time: 919.350937 msecs"
+;; 811
+
+;; not practical
