f653705cac6712d4b84012bce511dba989985645
Karsten Lang <karstenlang@gmail.com>
2010-02-03 19:11:46 +0100

    not done .. just some ideas

---
+++ src/problem099.clj
@@ -0,0 +1,33 @@
+(load "tools")
+(use 'clojure.contrib.duck-streams)
+
+;;(> (expt 632382 518061) (expt 519432 525806))
+;; each number contains more than 3 million digits
+;; don't even think about evaluating the term.
+
+;; maybe the factors can be used for something?
+;; user> (factors 632382)
+;; [2 3 105397]
+;; user> (factors 518061)
+;; [3 172687]
+;; 
+;; user> (factors 519432)
+;; [2 2 2 3 23 941]
+;; user> (factors 525806)
+;; [2 19 101 137]
+
+(def base-exp (slurp "base_exp.txt"))
+(map #(list
+       (factors (. Integer parseInt (nth % 0) 10)) 
+       (factors (. Integer parseInt (nth % 1) 10))) 
+     (take 5 (map #(split % #",") (split base-exp #"\r\n"))))
+
+(defn factor-base-exp [be]
+  (let [base  (. Integer parseInt (nth be 0) 10)
+	exp   (. Integer parseInt (nth be 1) 10)
+	basef (factors base)
+	expf  (factors exp)]
+    (list (list base exp) basef expf)))
+
+(def factors-base-exp (map #(factor-base-exp %) 
+			   (map #(split % #",") (split base-exp #"\r\n"))))
