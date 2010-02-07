6e2e0088c81836779043d49cdd26c606954c279c
Karsten Lang <karstenlang@gmail.com>
2010-02-07 10:19:05 +0100

    done

---
+++ src/problem037.clj
@@ -0,0 +1,31 @@
+(load "tools")
+; The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+
+; Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+; NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+
+(def prime? (memoize prime?))
+
+(defn truncable-prime? [number]
+  (loop [ds (rest (digits number)) 
+	 sd (butlast (digits number))
+	 checks (sorted-set)
+	 ]
+    (if (= '() ds) 
+      (every? prime? checks)
+      (let [ids (integer ds)
+	    isd (integer sd)] 
+	(if (and (< 1 ids)
+		 (< 1 isd))
+	  (recur (rest ds) (butlast sd) (conj checks ids isd))
+	  false)))))
+
+(defn truncable-primes [n]
+    (drop 4 (take (+ n 4) (filter #(truncable-prime? %) primes))))
+
+;; user> (trunc-primes 11)
+;; (23 37 53 73 313 317 373 797 3137 3797 739397)
+;; user> (time (reduce + (truncable-primes 11)))
+;; "Elapsed time: 23647.350365 msecs"
+;; 748317
