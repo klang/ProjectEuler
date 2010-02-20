b49a4c8f69f840efb81bcbe07af5f967f0aad7d9
Karsten Lang <karstenlang@gmail.com>
2010-02-20 19:09:36 +0100

    done - problem 65

--- src/problem065.clj
+++ src/problem065.clj
@@ -15,18 +15,47 @@
 	  (let [r1 (quot d f), f1 (- d (* r1 f))]
 	    (recur  d f r1 f1 (conj cont-frac r1))))))))
 
+;(continued-fraction 415/93)
+;[4 2 6 7]
+;(+ 4 (/ 1 (+ 2 (/ 1 (+ 6 (/ 1 7))))))
+
 ; [2; 1,2,1, 1,4,1, 1,6,1 , ... , 1,2k,1, ...]
-; first 10
-;2, 3, 8/3, 11/4, 19/7, 87/32, 106/39, 193/71, 1264/465, 1457/536
 ;(take 10 (map (fn [k] [1 (* 2 k) 1]) (iterate inc 1)))
-(reduce concat (take 10 (map #(vector 1 (* 2 %) 1) (iterate inc 1))))
+;(reduce concat (take 10 (map #(vector 1 (* 2 %) 1) (iterate inc 1))))
 
+;; Returns a lazy sequence of the continued fraction representation of e. 
+;; e = (2; 1,2,1, 1,4,1, 1,6,1 , ... , 1,2k,1, ...)"
 (def continued-fraction-e 
-     (interleave (cycle [1]) (map #(* 2 %) (iterate inc 1)) (cycle [1])))
+     (lazy-cat '(2) 
+	       (interleave (cycle [1]) 
+			   (map #(* 2 %) (iterate inc 1)) 
+			   (cycle [1]))))
+;(take 30 continued-fraction-e)
 
-;; lazy sequence that first returns 2, then elements from continued-fraction-e
-;; 2
-;; 2 + 1
-;; 2 + 1 +
-(def e )
+; first 10
+; (2 1 2 1 1 4 1 1 6 1)
+; (2 3 8/3 11/4 19/7 87/32 106/39 193/71 1264/465 1457/536)
+;; (+ 2)
+;; (+ 2 (/ 1 1))
+;; (+ 2 (/ 1 (+ 1 (/ 1 2))))
+;; (+ 2 (/ 1 (+ 1 (/ 1 (+ 2 (/ 1 1))))))
+;; (+ 2 (/ 1 (+ 1 (/ 1 (+ 2 (/ 1 (+ 1 (/ 1 1))))))))
+;; (+ 2 (/ 1 (+ 1 (/ 1 (+ 2 (/ 1 (+ 1 (/ 1 (+ 1 (/ 1 4))))))))))
+;; (+ 2 (/ 1 (+ 1 (/ 1 (+ 2 (/ 1 (+ 1 (/ 1 (+ 1 (/ 1 (+ 4 (/ 1 1))))))))))))
+;; (+ 2 (/ 1 (+ 1 (/ 1 (+ 2 (/ 1 (+ 1 (/ 1 (+ 1 (/ 1 (+ 4 (/ 1 (+ 1 (/ 1 1))))))))))))))
+;; (+ 2 (/ 1 (+ 1 (/ 1 (+ 2 (/ 1 (+ 1 (/ 1 (+ 1 (/ 1 (+ 4 (/ 1 (+ 1 (/ 1 (+ 1 (/ 1 6))))))))))))))))
+;; 
+;(+ 4 (/ 1 (+ 2 (/ 1 (+ 6 (/ 1 7))))))
+;; function that returns a certain number of estimations of a cf representation
+(defn estimate-continued-fraction [fraction]
+  (+ (first fraction) 
+     (if (= '() (rest fraction))
+       0
+       (/ (estimate-continued-fraction (rest fraction))))))
 
+;; user> (estimate-continued-fraction (take 100 continued-fraction-e))
+;; 6963524437876961749120273824619538346438023188214475670667/2561737478789858711161539537921323010415623148113041714756
+;; (load "tools")
+;; user> (reduce + (digits (.numerator (estimate-continued-fraction (take 100 continued-fraction-e)))))
+;; 272
+;; 
