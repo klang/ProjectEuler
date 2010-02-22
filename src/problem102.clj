65698c02276b30cc3fdfad472cc140d36ed9bf12
Karsten Lang <karstenlang@gmail.com>
2010-02-22 19:41:18 +0100

    explanation

--- src/problem102.clj
+++ src/problem102.clj
@@ -9,7 +9,12 @@
   (map #(. Integer parseInt % 10) v))
 
 (def triangles (map #(str2int %) 
-		    (map #(split % ",") (split triangles-txt #"r\n"))))
+		    (map #(split % #",") (split triangles-txt #"\r\n"))))
 
+;http://en.wikipedia.org/wiki/Linear_equation#Two-point_form
+;; convert the two point form to the intercept form for each
+;; of the 3 lines in each triangle. 
+;; from that, it can be determined where the origin is in 
+;; relation to the lines
 
 (defn triangle-origin [[x1 y1 x2 y2 x3 y3]] nil)
