859586b83a4ebad1bcb1163bd374dfa3a384335e
Karsten Lang <karstenlang@gmail.com>
2010-02-24 22:59:42 +0100

    done - problem 80

--- src/problem080.clj
+++ src/problem080.clj
@@ -2,7 +2,14 @@
 (load "problem064")
 (load "problem065")
 
-(defn count-odd-periods-b [N]
-  (count (filter odd-period? 
-		 (map #(contiuned-fraction-for-sqrts %) 
-		      (take-while #(<= % N) not-perfect-squares)))))
+(use 'clojure.contrib.math)
+(= 475 (reduce + (take 100 (digits (first (exact-integer-sqrt (* 2 (expt 10 200))))))))
+
+(defn sum-digits [n]
+  (reduce + (take 100 (digits (first (exact-integer-sqrt (* n (expt 10 200))))))))
+
+(def irrational-squares
+     (filter (fn [n] (not (rational? (sqrt n)))) (iterate inc 1)))
+
+; (map #(sum-digits %) (take-while #(< % 100) irrational-squares))
+(reduce + (map #(sum-digits %) (take-while #(< % 100) irrational-squares)))
