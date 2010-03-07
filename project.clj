01fc91601a4ae16b8dc2afd636156d94267393c1
Karsten Lang <karstenlang@gmail.com>
2010-03-07 12:46:52 +0100

    changing the versions of clojure and clojure.contrib a bit

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ProjectEuler "0.1"
   :description "Problems solved on http://projecteuler.net"
   :url "http://github.com/klang/ProjectEuler"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
 		 ]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
 
