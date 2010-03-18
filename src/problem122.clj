0d7f5c4f356aa8d4fe015095b5042ee3e4b0c8b0
Karsten Lang <karstenlang@gmail.com>
2010-03-18 17:07:04 +0100

    python version added - must translate to Clojure

--- src/problem122.clj
+++ src/problem122.clj
@@ -86,3 +86,17 @@
 ; (1582 1688 1635 1584)
 ; 1583 not it
 ; 1582 correct .. 1 not counted then
+
+;; python:
+;; lim = 200
+;;  
+;; best = [None, [set([1])]]
+;; for exp in range(2, lim + 1):
+;;     facts = []
+;;     for f1 in xrange(1, exp / 2 + 1):
+;;         for constr in best[exp - f1]:
+;;             if f1 in constr: facts.append(constr.union([exp]))
+;;     bestlen = min(len(fact) for fact in facts)
+;;     best.append([fact for fact in facts if len(fact) == bestlen])
+;;  
+;; print sum(len(b[0]) - 1 for b in best[1:])
