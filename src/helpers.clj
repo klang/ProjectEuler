fa7b57fcebc5a7449613d7236eef996e19a5edf2
Karsten Lang <karstenlang@gmail.com>
2010-03-21 17:04:16 +0100

    including stuff

--- src/helpers.clj
+++ src/helpers.clj
@@ -1,4 +1,6 @@
-(ns helpers)
+(use 'clojure.contrib.math 'clojure.contrib.test-is)
+(load "tools")
+
 ;; stolen method:
 ;; integer-length defined as a private function in clojure.contrib.math
 ; Length of integer in binary, used as helper function for sqrt.
@@ -9,3 +11,4 @@
   (count (Long/toBinaryString n)))
 (defmethod integer-length java.math.BigInteger [n]
   (count (. n toString 2)))
+
