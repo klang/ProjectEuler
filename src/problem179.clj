01f76e467f23297ae890b75287e94151e299adfb
Karsten Lang <karstenlang@gmail.com>
2010-03-31 19:16:01 +0200

    problem 179 - comparing dorun/for/transient, loop/recur/transient, dorun/for/int-array and loop/recur/int-array

--- src/problem179.clj
+++ src/problem179.clj
@@ -4,7 +4,7 @@
 ;(load "tools")
 (ns p179
   (:use	[tools.primes :only (divisors#)]
-	[clojure.contrib.math :only (expt)]))
+	[clojure.contrib.math :only (expt exact-integer-sqrt)]))
 
 (defn f [limit]
   (loop [n 2, dn (divisors# n), c 0]
@@ -55,13 +55,116 @@
 ;; "Elapsed time: 10575.103723 msecs"
 ;; 10585
 
-;; still not efficient, but at least it doesn't run out of memeory.
+;; still not efficient, but at least it doesn't run out of memeory. (64MB)
 ;; .. and I am sure there is no run-away subset calculations on the way...
 
 ;; p179> (time (f1 (expt 10 7)))
 ;; "Elapsed time: 4343526.044964 msecs"
 ;; 986262
 
+(def p179limit10
+     (let [v (transient (vec (repeat 10 1)))] ;  [1 1 1 1 1 1 1 1 1 1])
+       (assoc! v 2 (+ (v 2) 1))		; 2
+       (assoc! v 4 (+ (v 4) 1))
+       (assoc! v 6 (+ (v 6) 1))
+       (assoc! v 8 (+ (v 8) 1))
+       (assoc! v 3 (+ (v 3) 1))		; 3
+       (assoc! v 6 (+ (v 6) 1))
+       (assoc! v 9 (+ (v 9) 1))		; 4
+       (assoc! v 4 (+ (v 4) 1))
+       (assoc! v 8 (+ (v 8) 1))
+       (assoc! v 5 (+ (v 5) 1))		; 5
+       (assoc! v 6 (+ (v 6) 1))		; 6
+       (assoc! v 7 (+ (v 7) 1))		; 7
+       (assoc! v 8 (+ (v 8) 1))		; 8
+       (assoc! v 9 (+ (v 9) 1))		; 9
+       (persistent! v)))
+
+(def p179limit10print
+     (for [i (range 2 10) j (range i 10 i)] 
+       (str "(assoc! v "j" (+ (v "j") 1))\n")))
+
+(def p179limit10hard
+     (let [v (transient (vec (repeat 10 1)))]
+       (doall
+	(for [i (range 2 10) j (range i 10 i)] 
+	  (assoc! v j (+ (v j) 1))))
+       (persistent! v)))
+
+; (println p179limit10print)
+
+(defn p179-dorun-transient [limit]
+  (let [v (transient (vec (repeat limit 1)))]
+    (dorun
+     (for [i (range 1 limit) j (range i limit i)] 
+       (assoc! v j (+ (v j) 1))))
+    (persistent! v)))
+
+;(def v (p179 (expt 10 3)))
+;(let [w (cons 0 v)] (reduce + (map #(if (= %1 %2) 1 0) v w)))
+
+(defn p179res [limit]
+  (let [v (transient (vec (repeat limit 1)))]
+    (dorun
+     (for [i (range 1 limit) j (range i limit i)] 
+       (assoc! v j (+ (v j) 1))))
+    (let [v (persistent! v)
+	  w (cons 0 v)] (reduce + (map #(if (= %1 %2) 1 0) v w)))))
+
+(defn p179-recur-transient [limit]
+  (loop [i 0 d 2 divs (transient (vec (repeat limit 1)))]
+    (if (= d limit) 
+      (persistent! divs)
+      (if (>= i limit) 
+	(recur (int 0) (inc d) divs)
+	(recur (+ i d) d (assoc! divs i (+ (divs i) 1)))))))
+
+(defn p179-dorun-int-array [limit]
+  (let [v (int-array limit 1)]
+    (dorun
+     (for [i (range 1 limit) j (range i limit i)] 
+       (aset v j (+ (aget v j) 1)))) v))
+
+;; A different take on the problem. (inspiring some of the above versions)
+;; Now, why does this use less memory as well as being faster?
+;; http://github.com/killy971/project-euler/blob/master/clojure/net/projecteuler/179.clj
+(defn make-divs-seq [limit]
+  (loop [i (int 0), d (int 2), divs (int-array limit 1)]
+    (if (= d limit) 
+      divs
+      (if (>= i limit) 
+	(recur (int 0) (inc d) divs)
+	(recur (+ i d) d (do (aset divs i (inc (aget divs i))) divs))))))
+ 
+(defn same-cnt [divs]
+  (let [divs2 (cons 0 (seq divs))]
+    (reduce + (map #(if (= %1 %2) 1 0) divs divs2))))
+ 
+(defn problem-179 []
+  (let [divs-seq (make-divs-seq (int 1e7))]
+    (same-cnt divs-seq)))
+
+;; p179> (time (problem-179))
+;; "Elapsed time: 38507.965323 msecs"
+;; 986262
+
+(deftest test-limited-end-result-time
+  (let [limit 1000]
+    (is (= (time (f1 limit))
+	   (time (p179res limit))
+	   (time (same-cnt (p179-dorun-transient limit)))	
+	   (time (same-cnt (p179-recur-transient limit)))
+	   (time (same-cnt (p179-dorun-int-array limit)))
+	   (time (same-cnt (take limit (make-divs-seq limit))))))))
+
+(deftest test-resulting-vector
+  (let [limit 100]
+    (is (= (p179-dorun-transient limit)
+	   (vec (take limit (p179-dorun-int-array limit)))))
+    (is (= (p179-recur-transient limit) 
+	   (vec (take limit (make-divs-seq limit)))))))
 
 
-  
+
+
+
