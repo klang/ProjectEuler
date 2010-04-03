4666f941b877f99222ecf7b58ad955fe9bf004b5
Karsten Lang <karstenlang@gmail.com>
2010-04-03 15:13:27 +0200

    problem 18 - done

--- src/problem018.clj
+++ src/problem018.clj
@@ -9,44 +9,121 @@
 ;; 
 ;; Find the maximum total from top to bottom of the triangle below:
 ;; 
-;;               75
-;;              95 64
-;;             17 47 82
-;;            18 35 87 10
-;;           20 04 82 47 65
-;;          19 01 23 75 03 34
-;;         88 02 77 73 07 63 67
-;;        99 65 04 28 06 16 70 92
-;;       41 41 26 56 83 40 80 70 33
-;;      41 48 72 33 47 32 37 16 94 29
-;;     53 71 44 65 25 43 91 52 97 51 14
-;;    70 11 33 28 77 73 17 78 39 68 17 57
-;;   91 71 52 38 17 14 91 43 58 50 27 29 48
-;;  63 66 04 68 89 53 67 30 73 16 69 87 40 31
-;; 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+;; [snipped]
 ;; 
 ;; NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
-
-
-(def triangle-txt (slurp "triangle-018.txt"))
+(ns p018
+  (:use clojure.test)
+  (:use [clojure.contrib.str-utils2 :only (split)]))
 
 (defn str2int [v]
   (map #(. Integer parseInt % 10) v))
 
-(def triangle (into [] (map #(into [] ( str2int %)) 
-			    (map #(split % #" ") (split triangle-txt #"\n")))))
+(defn read-data [filename]
+  (into [] (map #(into [] (str2int %)) 
+		(map #(split % #" ") (split (slurp filename) #"\n")))))
 
-(def t [[3] [7 4] [2 4 6] [8 5 9 3]])
+(def t-hard [[3] [7 4] [2 4 6] [8 5 9 3]])
+(def t-read (read-data "triangle-018-test.txt"))
+(def t-full (read-data "triangle-018.txt"))
+(def t-mega (read-data "triangle.txt"))
+(deftest test-read-data (is (= t-read t-hard)))
 
-(defn edges [G]
-  "return all the edges of G"
-  (for [l (range 0 (- (count G) 1)) 
-	i (range 0 (count (G l))) 
-	j (range i (+ i 2))] 
-    [l i j]))
+(max (+ 3 7 2) (+ 3 7 4) (+ 3 4 4) (+ 3 4 6))   ; lookahead = 2 elements
+;;      0 0 0    [0 0 1]    0 1 1     0 1 2     ; item number
 
-(defn value [G [l i j]]
-  "return the value of the edge"
-  (vector (nth (G l) i) (nth (G (+ l 1)) j)))
+(max (+ 3 7 2 8) (+ 3 7 2 5) (+ 3 7 4 5) (+ 3 7 4 9 ) (+ 3 4 4 5) (+ 3 4 4 9) (+ 3 4 6 9) (+ 3 4 6 3))
+;;      0 0 0 0     0 0 0 1     0 0 1 1    [0 0 1 2]     0 1 1 1     0 1 1 2     0 1 2 2     0 1 2 3
 
-(map #(value triangle %) (edges triangle))
+;;      0 0 0    [0 0 1]    0 1 1     0 1 2     ; item number
+(max (+ 3 7 2) (+ 3 7 4) (+ 3 4 4) (+ 3 4 6))   ; lookahead = 2 elements
+
+(def t002 {[0 0 0] [3 7 2] [0 0 1] [3 7 4] [0 1 1] [3 4 4] [0 1 2] [3 4 6]})
+(def c (into {} (map #(vector % (reduce + (t002 %))) (keys t002))))
+
+(defn lookahead-max [triangle] nil)
+
+(defn path-elements  [triangle level element lookahead])
+
+(defn paths [triangle level element lookahead]
+  "paths going out from an 'element on 'level of length 'lookahead"
+  ;; paths going out from 'element and 'element on 'level+1 of length 'lookahead-1
+  ;(cond (zero? lookahead) ())
+  )
+
+(defn reduce-1-level [triangle]
+  (cons (into [] (for [a (first triangle)
+		       b (second triangle)] (+ a b))) (drop 2 triangle)))
+
+(def triangle [[3] [7 4] [2 4 6] [8 5 9 3]])
+(def level1 (first triangle))
+(def level2 (second triangle))
+(for [i (range 0 (count level1))] 
+  (list (+ (level1 i) (level2 i)) 
+	(+ (level1 i) (level2 (+ i 1)))))
+
+(def triangle [[10 7] [2 4 6] [8 5 9 3]])
+(def level1 (first triangle))
+(def level2 (second triangle))
+(for [i (range 0 (count level1))] 
+  (list (+ (level1 i) (level2 i)) 
+	(+ (level1 i) (level2 (+ i 1)))))
+
+(def triangle [[12 14 13] [8 5 9 3]])
+(def level1 (first triangle))
+(def level2 (second triangle))
+
+(for [i (range 0 (count level1))] 
+  (list (+ (level1 i) (level2 i)) 
+	(+ (level1 i) (level2 (+ i 1)))))
+
+(def triangle [[12 14 13] 
+	       [8 5 9 3]])
+
+(def level1 (first triangle))
+(def level2 (second triangle))
+
+(defn level2-max [level1 level2]
+  ;; (list (max (+ 5 12) (+ 5 14)) (max (+ 9 14) (+ 9 13)))
+  "constructs a level2 with max taken between each element and the two elements 'above' from level 1. first and last element will be missing. see next-level2"
+  (for [i (range 1 (- (count level2) 1))] 
+    (max (+ (level1 i) (level2 i)) 
+	 (+ (level1 (- i 1)) (level2 i)))))
+
+;; TODO: find a better way to create a vector from several parts
+(defn next-level2 [level1 level2]
+  "constructs a new level2 based on level1 and level2. Edge elements are glued onto the result from level2-max."
+  (vec (concat (into  [(+ (first level1) (first level2))]
+		      (level2-max level1 level2))
+	       [(+ (last level1) (last level2))])))
+
+(deftest test-next-level2
+  (is (= (next-level2 [3] [7 4]) [10 7]))
+  (is (= (next-level2 [10 7] [2 4 6]) [12 14 13]))
+  (is (= (next-level2 [12 14 13] [8 5 9 3]) [20 19 23 16])))
+
+(defn squeeze-max [triangle]
+  "squeezes the first two levels of a triangle into a new triangle with running maxes calculated for each element"
+  (let [level1 (first triangle)      ;  [2 4 6]
+	level2 (second triangle)     ; [8 5 9 3]
+	next-level (next-level2 level1 level2)]
+    (into [next-level2] (drop 2 triangle))))
+
+(defn squeeze-max [triangle]
+  "squeezes the first two levels of a triangle into a new triangle with running maxes calculated for each element"
+  (into [(next-level2 (first triangle) (second triangle))] (drop 2 triangle)))
+
+;(reduce max (first (squeeze-max (squeeze-max (squeeze-max triangle)))))
+
+(defn squeeze-max-triangle [triangle]
+  (loop [triangle (squeeze-max triangle)]
+    (if (= 1 (count triangle))
+      ;(do (println triangle)) 
+      (reduce max (first triangle))
+      ;(do (println triangle))
+      (recur (squeeze-max triangle)))))
+
+;; p018> (time (squeeze-max-triangle t-full))
+;; "Elapsed time: 0.885029 msecs"
+;; 1074
+
