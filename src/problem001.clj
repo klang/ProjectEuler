85044b401b64ecf33a3b1499faa9ee479e3ce00c
Karsten Lang <karstenlang@gmail.com>
2010-04-03 15:26:57 +0200

    problem001 - renaming namespace

--- src/problem001.clj
+++ src/problem001.clj
@@ -4,7 +4,7 @@
 
 ;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-(ns p001
+(ns problem001
   (:use clojure.test))
 
 (defn problem001 [n]
