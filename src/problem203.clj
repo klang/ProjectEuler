46d62c5376f4e446cad74fb97b872a22f5e1bdf9
Karsten Lang <karstenlang@gmail.com>
2010-04-10 19:09:43 +0200

    done - problem 203 - not really efficient

--- src/problem203.clj
+++ src/problem203.clj
@@ -1,15 +1,12 @@
-;; http://www.research.att.com/~njas/sequences/A000984
-
-;; C(2n,n) = (2n)!/(n!)^2.
-(defn C [n]
-  (/ (factorial (* 2 n)) (expt (factorial n) 2)))
-
-;It can indeed be solved in one equation, and curiously enough, the solution to it is in one of the other problems. The solution lies in the N choose R formula. For any number of rows R, you take (2R!)/(R! ^ 2). This comes from the equation n choose r = n!/r!(n-r)!, and you replace n with 2r, for the number of rows problem. :) 
-
-;P.S. I liked the hint "rth element in the nth row"! ;)
+(ns problem203
+  (:use [tools.primes :only (divisors# prime-factors divisors)]
+	[tools.pseudo-primes :only (next-prime prime?)]
+	[tools.numbers :only (factorial)]
+	[clojure.contrib.math :only (expt exact-integer-sqrt)]
+	[clojure.contrib.lazy-seqs :only (primes)]
+	[clojure.test]))
 
 ;; from http://rosettacode.org/wiki/Pascal's_triangle#Clojure
-
 (defn nextrow [row]
   (vec (concat [1] (map #(apply + %) (partition 2 1 row)) [1] )))
  
@@ -19,11 +16,56 @@
     (doseq [row triangle]
       (println row))))
 
-(def the51st [1 50 1225 19600 230300 2118760 15890700 99884400 536878650 2505433700 10272278170 37353738800 121399651100 354860518600 937845656300 2250829575120 4923689695575 9847379391150 18053528883775 30405943383200 47129212243960 67327446062800 88749815264600 108043253365600 121548660036300 126410606437752 121548660036300 108043253365600 88749815264600 67327446062800 47129212243960 30405943383200 18053528883775 9847379391150 4923689695575 2250829575120 937845656300 354860518600 121399651100 37353738800 10272278170 2505433700 536878650 99884400 15890700 2118760 230300 19600 1225 50 1])
+;;problem203> (reduce into #{} (take 8 (iterate nextrow [1])))
+;;#{1 2 3 35 4 5 6 7 10 15 20 21}
+;;problem203> (count  (reduce into #{} (take 8 (iterate nextrow [1]))))
+;;12
 
-;; (use '[clojure.contrib.math :only (expt exact-integer-sqrt)])
-;; (exact-integer-sqrt 126410606437752)
-;; user> (exact-integer-sqrt 126410606437752)
-;; [11243247 3334743]
-;; user> (exact-integer-sqrt 11243247)
-;; [3353 638]
+(defn distinct-pascal [n]
+  (reduce into #{} (take n (iterate nextrow [1]))))
+
+(defn square-free-pascal [n]
+  (loop [p primes
+	 dp (distinct-pascal n)]
+    (if (< (reduce max dp) (expt (first p) 2)) (list (first p) dp)
+	(recur (rest p) (filter #(not (zero? (rem % (expt (first p) 2)))) dp)))))
+
+(comment
+  (reduce + (square-free-pascal 8)))
+
+;; problem203> (filter prime? (distinct-pascal 51))
+;; (2 3 5 37 7 41 11 43 13 47 17 19 23 29 31)
+
+;; problem203> (time (def dp49 (square-free-pascal 49)))
+;; "Elapsed time: 187455.591571 msecs"
+;; problem203> (difference (set (second dp49)) (set dp48))
+;; #{}
+;; problem203> (first dp49)
+;; 3124519
+;; problem203> (time (def dp50 (square-free-pascal 50)))
+;; "Elapsed time: 186927.913462 msecs"
+;; problem203> (difference (set (second dp50)) (set (second dp49)))
+;; #{}
+;; problem203> (first dp50)
+;; 3124519
+;; problem203> (time (def dp51 (square-free-pascal 51)))
+;; "Elapsed time: 206650.517801 msecs"
+;; #'problem203/dp51
+;; problem203> (first dp51)
+;; 3124519
+;; problem203> (difference (set (second dp51)) (set (second dp50)) )
+;; #{}
+;; problem203> (reduce + (second dp51))
+;; 34029210557338
+
+;;..... there is always a better way ... 
+
+;; C(n,k)
+(defn C [n k]
+  (/ (factorial n) (* (factorial k) (factorial (- n k)))))
+
+;; [daniel.is.fischer] Any prime dividing C(n,k) is ≤ n, so you need only the primes up to 47.
+
+;; [MarleysGhost] I was surprised at how few primes were necessary. By the time even the largest elements of the triangle were divided by primes up through 43, they were reduced to less than 47*47.
+;; [quilan] Well, yeah... highest numerator term [ n! / (k!*(n-k)!) ] is going to be 51!, so the highest prime-factor will have to be <= 51. Therefore, 47.
+
