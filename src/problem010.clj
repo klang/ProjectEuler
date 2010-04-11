da50f25fe83d4c58ed6d74c396a7cf45d0f6b872
Karsten Lang <karstenlang@gmail.com>
2010-04-11 09:59:04 +0200

    problem010 - introducing namespace

--- src/problem010.clj
+++ src/problem010.clj
@@ -1,8 +1,10 @@
+(ns problem010
+  (:use	[clojure.contrib.lazy-seqs :only (primes)]
+	[clojure.test]))
+
 ; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 ; Find the sum of all the primes below two million.
-
-(use 'clojure.contrib.test-is 'clojure.contrib.lazy-seqs)
 
 (defn problem010 [n]
   (reduce + (take-while #(< % n) primes)))
@@ -10,7 +12,7 @@
 (deftest test-problem010
   (is (= 17 (problem010 10))))
 
-; user> (time (problem010 2000000))
+; problem010> (time (problem010 2000000))
 ; "Elapsed time: 46393.526973 msecs"
 ; 142913828922
 
