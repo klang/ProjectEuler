e69da84accd5c74e0d8134d8849bdeeb8fb95cfb
Karsten Lang <karstenlang@gmail.com>
2010-04-12 21:52:36 +0200

    problem038 - stab, stab

--- src/problem038.clj
+++ src/problem038.clj
@@ -1,8 +1,35 @@
-(load "tools")
-(use 'clojure.contrib.combinatorics)
+(ns problem038
+  (:use clojure.contrib.combinatorics
+	tools.numbers
+	clojure.test))
 
-(count  (permutations '(1 2 3 4 5 6 7 8 9)))
+;;Take the number 192 and multiply it by each of 1, 2, and 3:
+
+;; 192 x 1 = 192
+;; 192 x 2 = 384
+;; 192 x 3 = 576
+;; By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+;; 
+;; The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+;; 
+
+;; What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
+(deftest test-examples
+  (is (= 9 (count (reduce concat (map digits (map #(* 192 %) '(1 2 3)))))))
+  (is (= 9 (count (reduce concat (map digits (map #(* 9 %) '(1 2 3 4 5))))))))
+
+(defn pandigital? [number]
+  (= 9 (count (distinct (digits number)))))
+
+(def perms  (map integer (permutations '(9 8 7 6 5 4 3 2 1))))
+;; number of permutations is of course (factorial 9)
+
 
 ;; find a permutation, p that can be split in n pieces, where
-;; (= (reduce + (* n piece-n)) p)
+(defn task [p m n]
+  (and (= (digits p) 
+	  (reduce concat (map digits (map #(* m %) (range 1 (+ n 1)))))) 
+       (pandigital? p)))
 
+(deftest test-task
+  (is (task 192384576 192 3)))
