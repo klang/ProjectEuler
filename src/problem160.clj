d0dda0ff7db5dfb95910b7ef8979c1a12324e071
Karsten Lang <karstenlang@gmail.com>
2010-04-15 22:09:53 +0200

    problem 160 - the right track

--- src/problem160.clj
+++ src/problem160.clj
@@ -46,3 +46,71 @@
 
 ;;problem160> (factorial-digits 12)
 ;;2147483647
+
+;; http://en.wikipedia.org/wiki/Trailing_zeros#Factorial
+(defn trailing-zeroes [n]
+  (reduce + (map #(floor (/ n (expt 5 %))) 
+		 (take-while #(>= n (expt 5 %)) 
+			     (iterate inc 1)))))
+
+;; problem160> (trailing-zeroes (expt 10 12))
+;; 249999999997
+
+(defn f1 [n]
+  (let [a (trailing-zeroes n)
+	b1 (expt 10 a)
+	b2 (* (expt 10 5) b1)]
+    (quot (mod (factorial n) b2) b1)))
+
+;; f1 is much faster than f, but the expensive factorial operation is still done
+
+(deftest test-f1
+  (is (= 36288 (f1 9)))
+  (is (= 36288 (f1 10)))
+  (is (= 17664 (f1 20))))
+
+
+;; http://www.luschny.de/math/factorial/FastFactorialFunctions.htm
+
+;; problem160> (time (f1 10000))
+;; "Elapsed time: 18271.535222 msecs"
+;; 79008
+
+(defn special-factorial [n] 
+  (reduce * (range n 0 -1)))
+
+(defn s [next so-far] (mod (* next so-far) (expt 10 5)))
+
+(defn f2 [n]
+  (let [ex (expt 10 5)]
+    (loop [current (range 10 (+ n 1))
+	   ;current (range 100000 (+ n 1))
+	   ;so-far 62496
+	   so-far 36288
+	   ]
+      (if (empty? current)
+	so-far
+	(let [candidate (mod (* (first current) so-far) ex)
+	      check (mod candidate 10)]
+	  (recur (rest current) (if (zero? check) so-far candidate)))))))
+
+(deftest test-f-f1
+  (is (every? true? (map #(= (f %) (f1 %)) (range 1 100)))))
+
+(deftest test-f-f1-f2
+  (is (every? true? (map #(= (f %) (f1 %) (f2 %)) (range 10 17)))))
+
+(deftest test-f-f1-f2-short
+  (is (= (f 15) (f1 15) (f2 15)))
+  (is (= (f 16) (f1 16) (f2 16))))
+
+;; problem160> (time (f2 100000))
+;; "Elapsed time: 0.29473 msecs"
+;; 62496
+;; problem160> (time (f2 1000000))
+;; "Elapsed time: 2244.540732 msecs"
+;; 62496
+;; problem160> (time (f2 10000000))
+;; "Elapsed time: 28711.281465 msecs"
+;; 62496
+;; not the correct answer..
