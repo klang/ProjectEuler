1d90c96e42e10b56932be8bb424e2b74b4ff3ec0
Karsten Lang <karstenlang@gmail.com>
2010-04-23 16:00:30 +0200

    done - problem 120

--- src/problem120.clj
+++ src/problem120.clj
@@ -18,46 +18,26 @@
 ;; problem120> (maximum 7)
 ;; 42
 
-;; problem120> (time (reduce + (map maximum (range 3 300))))
-;; "Elapsed time: 10308.482343 msecs"
-;; 8887850
-;; problem120> (time (reduce + (map maximum (range 300 350))))
-;; "Elapsed time: 8980.675778 msecs"
-;; 5251100
-;; problem120> (time (reduce + (map maximum (range 340 400))))
-;; "Elapsed time: 14626.873735 msecs"
-;; 8176570
-
 ;; a little faster
 (defn maximum [n]
   (reduce #(max %1 %2) (map #(euler n %) (range 1 (* 2 n)))))
-
-;; problem120> (time (reduce + (map maximum (range 400 450))))
-;; "Elapsed time: 18746.160445 msecs"
-;; 8988600
-;; problem120> (time (reduce + (map maximum (range 450 500))))
-;; "Elapsed time: 27474.452925 msecs"
-;; 11232350
 
 ;; faster still .. as we notice that euler = 2 for even values
 (defn maximum [n]
   (reduce #(max %1 %2) (map #(euler n %) (range 1 (* 2 n) 2))))
 
-;; problem120> (time (reduce + (map maximum (range 500 600))))
-;; "Elapsed time: 43799.653571 msecs"
-;; 30195950
-;; problem120> (time (reduce + (map maximum (range 700 750))))
-;; "Elapsed time: 51037.604835 msecs"
-;; 26201100
-;; problem120> (time (reduce + (map maximum (range 750 800))))
-;; "Elapsed time: 62652.631227 msecs"
-;; 29944850
-;; problem120> (time (reduce + (map maximum (range 800 900))))
-;; "Elapsed time: 174200.663861 msecs"
-;; 72120950
-;; problem120> (time (reduce + (map maximum (range 900 1001))))
-;; "Elapsed time: 251907.554752 msecs"
-;; 91093950
+;; problem120> (time (reduce + (map maximum (range 3 1001))))
+;; "Elapsed time: 608337.113359 msecs"
+;; 333082500
 
-;; (+ 8887850 5251100 8176570 8988600 11232350 30195950 26201100 29944850 72120950 91093950)
-;; 292093270 <-- incorrect
+;; (a+1)^n == an+1 (mod a^2), and (a-1)^n == an-1 or 1-an (mod a^2) depending whether n is odd or even; the sum is therefore either 2an or 2. 
+
+;; When a is odd, this is always maximised at a^2-a (as in the example with a=7), achieved for example when n=(a-1)/2; when a is even, it is maximised at a^2-2a for a>2, achieved for example when n=(a-2)/2. 
+
+;; The separate sums for odd and even terms are easily enough calculated, but it was quicker to knock out a line of code: 
+
+;; perl -wle '$s+=$_*($_-2+($_&1)) for 3..1000; print $s' 
+
+(
+ (reduce + (map #(- (expt % 2) (* (- 2 (mod % 2)) %)) (range 3 1001)))
+)
