e6853e38958ed2aa36f3ebcd4c03a38c3629c720
Karsten Lang <karstenlang@gmail.com>
2010-04-25 13:24:31 +0200

    problem 206 -- something is seriously wrong

--- src/problem206.clj
+++ src/problem206.clj
@@ -45,6 +45,12 @@
 (defn d3? [number] (= 3 (quot (rem number 1000000000000000)     100000000000000))) ;; (expt 10 8)
 (defn d2? [number] (= 2 (quot (rem number 100000000000000000)   10000000000000000))) ;; (expt 10 8)
 (defn d1? [number] (= 1 (quot (rem number 10000000000000000000) 1000000000000000000))) ;; (expt 10 8)
+(defn dall? [n] (and (d0? n) (d9? n) (d8? n) (d7? n) (d6? n) (d5? n) (d4? n) (d3? n) (d2? n) (d1? n)))
+(deftest test-d
+  (is (d0? 1929394959697989990)) (is (d9? 1929394959697989990)) (is (d8? 1929394959697989990)) (is (d7? 1929394959697989990))
+  (is (d6? 1929394959697989990)) (is (d5? 1929394959697989990)) (is (d4? 1929394959697989990)) (is (d3? 1929394959697989990))
+  (is (d2? 1929394959697989990)) (is (d1? 1929394959697989990)) (is (dall? 1929394959697989990)))
+
 
 (comment
   (time (count (filter #(and (d0? %) (d9? %) (d8? %) (d7? %) 
@@ -53,6 +59,8 @@
 ;;"Elapsed time: 10751.909501 msecs"
 ;; 0
 ;;(filter #(and (d0? % ) (d9? %) (d8? %) (d7? %) (d6? %) (d5? %) (d4? %) (d3? %)) (map #(* % %) (take 1000000 (drop 1000000 total-range))))
+;;problem206> (take 1 (map sqrt (filter #(and (d3? %) (d4? %) (d5? %) (d6? %) (d7? %) (d8? %) (d9? %)) (map #(* % %) total-range-10))))
+;;(1022420830)
 
 ;; we need some higher jumps ..
 ;; to get 9 in the third position AND 0 in the first position
@@ -76,8 +84,8 @@
 (defn jump-cycle [n]
   (loop [foo (map sqrt (filter #(and (d8? %) (d9? %) (d0? %)) (map #(* % %) (range 1 n)))) 
 	 catch []]
-    (println (first foo))
-    (if (= 1 (count foo))
+    ;(println (first foo))
+    (if (nil? (second foo))
       catch 
       (recur (rest foo) (conj catch (- (second foo) (first foo)))))))
 
@@ -85,9 +93,12 @@
   (loop [foo (take n (map sqrt (filter #(and (d8? %) (d9? %) (d0? %)) (map #(* % %) total-range)))) 
 	 catch [(first foo)]]
     ;(println (first foo))
-    (if (= 1 (count foo))
+    (if (nil? (second foo))
       catch 
       (recur (rest foo) (conj catch (- (second foo) (first foo)))))))
+
+(def total-range-10 (range (first (exact-integer-sqrt 1020304050607080900)) 
+			   (+ 1 (first (exact-integer-sqrt 1929394959697989990))) 10))
 
 ;; A jump-cycle that matches 8_9_0 found by trial and error 
 ;;(jump-cycle 7)
@@ -95,89 +106,38 @@
 ;; should return
 ;;{:start 1010101670 :cycle [100 300 840 300 100 860]}
 
-;; problem206> (time (f))
-;; (:2 1045344353617888900 1022420830 29568)
-;; (:2 1183384051687084900 1087834570 186561)
-;; (:2 1284324759677080900 1133280530 295631)
-;; (:2 1435334752697884900 1198054570 451089)
-;; (:2 1495354956607584900 1222847070 510591)
-;; (:2 1546334454677980900 1243516970 560198)
-;; (:2 1695394956697288900 1302073330 700734)
-;; (:2 1778354455647184900 1333549570 776277)
-;; (:2 2054364455667484900 1433305430 1015690)
-;; (:2 2096354959647480900 1447879470 1050668)
-;; (:2 2340304653657484900 1529805430 1247290)
-;; (:2 2343334954667980900 1530795530 1249667)
-;; (:2 2452314250617688900 1565986670 1334125)
-;; (:2 2489384458617588900 1577778330 1362426)
-;; (:2 2636384056647484900 1623694570 1472625)
-;; (:2 2734304757687288900 1653573330 1544334)
-;; "Elapsed time: 4602.730723 msecs"
-;; 
+; with d7?:  (jump-cycle 122)
+
 (defn f []
   (let [end (+ 1 (first (exact-integer-sqrt 1929394959697989990)))
-	start  (+ 20 (first (exact-integer-sqrt 1020304050607080900)))
-	jc (jump-cycle 7)]
+	;end (- (first (exact-integer-sqrt 1929394959697989990)) 3)
+	;end (- (first (exact-integer-sqrt 1929394959697989990)) 0)
+	end 989026640;1189026630
+	start (first (exact-integer-sqrt 1020304050607080900))
+	jc [end -10]
+	;jc [start 10]
+	;jc [end -1]
+	;jc [1350100950 10]
+	;jc (jump-cycle 7)
+	;jc (jump-cycle 122)
+	]
     (loop [current (first jc)
 	   jump (cycle (rest jc))
 	   limit 1]
       ;; as we start from 1010101030, we know that the third to last digit in
       ;; the square is 9 and the last is 0, if the fourth to last digit is not 8, we jump .. 
-      (if (< limit 2000000) ; (< current end)
+      (if (< limit 20000000)		; (< current end)
 	(let [square (* current current)]
-	  (if (d7? square)
-	    (if (d6? square)
-	      (if (d5? square)
-		(if (d4? square)
-		  (if (d3? square)
-		    (if (d2? square)
-		      square
-		      (do (println (list :2 square current limit))))
-		    ;(do (println (list :3 square current limit)))
-		    )
-		  ;(do (println (list :4 square current limit)))
-		  )
-		;(do (println (list :5 square current limit)))
-		)
-	      ;(do (println (list :6 square current limit)))
-	      )
-	    ;(do (println (list :7 square current limit)))
-	    )
+	  (if (and (d0? square) (d9? square) (d8? square) 
+		   (d7? square) (d6? square) (d5? square) (d4? square) (d3? square))
+	    (if (d2? square)
+	      square
+	      (do (println (list :2 square current limit)))))
 	  (recur (+ (first jump) current) (rest jump) (inc limit))
 	  )
-	(list current (- current start)  (expt current 2))))))
+	(list current (- current start) (- current end)  (expt current 2) (dall? (expt current 2)) (< current end))))))
 
-(comment 
-(if (d0? square)
-	    (if (d9? square)
-	      (if (d8? square)
-		(if (d7? square)
-		  (if (d6? square)
-		    (if (d5? square)
-		      (if (d4? square)
-			(if (d3? square)
-			  (if (d2? square)
-			    square
-			    (do (println (list :2 square current limit))
-				(recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-			  (do (println (list :3 square current limit))
-			      (recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-			(do (println (list :4 square current limit))
-			    (recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-		      (do (println (list :5 square current limit))
-			  (recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-		    (do (println (list :6 square current limit))
-			(recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-		  (do (println (list :7 square current limit))
-		      (recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-		(do (println (list :8 square current limit))
-		    (recur (+ (- 10 (mod current 10)) current) jump (inc limit))))
-	      (do (println (list :9 square current limit))
-		  (recur (+ 10 current) jump (inc limit))))
-	    (do ;(println (list :0 square current limit))
-		(recur (+ (- 10 (mod current 10)) current) jump (inc limit))
-		))
-)
+
 (comment
 (if (and (d8? square) (d7? square) (d6? square) (d5? square) 
 		  (d4? square) (d3? square) (d2? square) (d1? square)))
