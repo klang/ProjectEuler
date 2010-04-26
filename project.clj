90a4ded8bfcfc099d861ca0194c704cd657a882c
Karsten Lang <karstenlang@gmail.com>
2010-04-26 20:51:46 +0200

    updating to the latest versions of swank-clojure and lein-swank

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject ProjectEuler "0.1"
+(defproject ProjectEuler "0.2"
   :description "Problems solved on http://projecteuler.net"
   :url "http://github.com/klang/ProjectEuler"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
 		 ]
-  :dev-dependencies [[org.clojure/swank-clojure "1.0"]
-		     [leiningen/lein-swank "1.0.0-SNAPSHOT"]
+  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
+		     [leiningen/lein-swank "1.2.0-SNAPSHOT"]
 		     [clojure_refactoring "0.1.3"]]
   )
 
