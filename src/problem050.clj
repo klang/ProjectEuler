522b7efba767a046819b8079fd9508d7556e15bb
Karsten Lang <karstenlang@gmail.com>
2010-04-28 22:54:07 +0200

    done - problem 50 (as the 80th problem)

--- src/problem050.clj
+++ src/problem050.clj
@@ -14,8 +14,16 @@ Which prime, below one-million, can be written as the sum of the most consecutiv
      :idea "use the old divisors trick, maybe?"
 }
   (:use tools.primes)
-  (:use clojure.contrib.lazy-seqs))
+  (:use clojure.contrib.lazy-seqs)
+  (:use clojure.test))
 
+(deftest test-problem050
+  (is (prime? (reduce + (take 21 (drop 3 primes)))) )
+  (is (= (filter prime? (filter #(< % 1000) (map #(- % (nth psums 2) ) psums)))
+	 '(7 31 67 271 491 953))
+      )
+  (is (= 953 (last (filter prime? (filter #(< % 1000) (map #(- % (nth psums 2) ) psums))))))
+  )
 ;; 2 2+3 2+3+5 2+3+5+7 2+3+5+7+11 2+3+5+7+11+13 ...
 ;;         i-1   i
 ;;           [i]-[i-1] sum starting from i
@@ -45,3 +53,57 @@ Which prime, below one-million, can be written as the sum of the most consecutiv
 
 ;; problem050> (prime? 37550402023)
 ;; true
+
+(defn make-prime-sum-seq [plimit]
+  (loop [p (rest plimit)
+	 sums [(first plimit)]]
+    (if (= '() p) 
+      sums
+      (recur (rest p) (conj sums (+ (sums (-  (count sums) 1)) (first p)))))))
+
+(comment
+  (def plimit (take-while #(< % 1000) primes))
+  (def psums (make-prime-sum-seq plimit))
+  (map (fn [i] [i (last (filter prime? (filter #(< % 1000) (map #(- % (nth psums i) ) psums))))]) (range 1 10))
+  (def counts (drop-last
+	       (map (fn [i] [i (last (filter prime? (filter #(< % 1000) (map #(- % (nth psums i)) psums))))]) 
+		    (range 1 (count psums))))))
+(comment
+  (def pnum 1000000)
+  (def plimit (take-while #(< % pnum) primes))
+  (def psums (make-prime-sum-seq plimit))
+  (def counts (drop-last 
+	       (map (fn [i] [i (last (filter prime? (filter #(< % pnum) (map #(- % (nth psums i)) psums))))]) 
+		    (range 1 (count psums)))))
+  (def plimiti (zipmap plimit (iterate inc 1)))
+  (def psumsi (zipmap psums (iterate inc 1)))
+  (def plengths (map (fn [[num sum]] [(- (psumsi (+ (nth psums num) sum)) num 1) sum]) (drop-last counts)))
+  )
+;; problem050> (time (count plimit-full))
+;; "Elapsed time: 20539.155341 msecs"
+;; 78498
+;; 40 seconds left to solve the problem
+
+(defn find-max-length [plengths]
+  (loop [p plengths
+	 mp [0 0]]
+    (if (= '() p)
+      mp
+      (if (< (first mp) (first (first p)))
+	(recur (rest p) (first p))
+	(recur (rest p) mp)))))
+
+(deftest test-find-max-length
+  (let [pnum 1000
+	plimit (take-while #(< % pnum) primes)
+	psums (make-prime-sum-seq plimit)
+	counts (drop-last 
+	       (map (fn [i] [i (last (filter prime? (filter #(< % pnum) (map #(- % (nth psums i)) psums))))]) 
+		    (range 1 (count psums))))
+	psumsi (zipmap psums (iterate inc 1))
+	plengths (map (fn [[num sum]] [(- (psumsi (+ (nth psums num) sum)) num 1) sum]) (drop-last counts))])
+  (is (= [21 953] (find-max-length plengths)))
+)
+
+;;problem050> (find-max-length (take 1000 plengths))
+;;[543 997651]
