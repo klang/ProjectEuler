68a35a952c0c0144cbf210b85d81f3d0efdc1251
Karsten Lang <karstenlang@gmail.com>
2010-04-29 18:10:43 +0200

    done - problem 31

--- src/problem031.clj
+++ src/problem031.clj
@@ -15,55 +15,42 @@ How many different ways can £2 be made using any number of coins?
 	x5p   (range 0 201 5)	x10p  (range 0 201 10)
 	x20p  (range 0 201 20)	x50p  (range 0 201 50)
 	x100p (range 0 201 100) x200p (range 0 201 200)
-	:when (= 200  (+ x1p x2p 
-		       x5p x10p x20p x50p x100p x200p)) ] 
+	:when (= 200  (+ x1p x2p x5p x10p x20p x50p x100p x200p)) ] 
     (list x1p x2p x5p x10p x20p x50p x100p x200p)))
 
 ;; takes incredibly long to execute.
 
 (defn change []
   (for [x1p   (range 0 201 1)   
-	x2p   (range 0 (+ (- 200 x1p) 1) 2)
-	x5p   (range 0 (+ (- 200 x1p x2p) 1) 5)	
-	x10p  (range 0 (+ (- 200 x1p x2p x5p) 1) 10)
-	x20p  (range 0 (+ (- 200 x1p x2p x5p x10p) 1) 20)
-	x50p  (range 0 (+ (- 200 x1p x2p x5p x10p x20p) 1) 50)
-	x100p (range 0 (+ (- 200 x1p x2p x5p x10p x20p x50p) 1) 100) 
-	x200p (range 0 (+ (- 200 x1p x2p x5p x10p x20p x50p x100p) 1) 200)
-	:when (= 200  (+ x1p x2p x1p x5p x10p x20p x50p x100p x200p)) ] 
+	x2p   (range 0 (- 201 x1p) 2)
+	x5p   (range 0 (- 201 x1p x2p) 5)	
+	x10p  (range 0 (- 201 x1p x2p x5p) 10)
+	x20p  (range 0 (- 201 x1p x2p x5p x10p) 20)
+	x50p  (range 0 (- 201 x1p x2p x5p x10p x20p) 50)
+	x100p (range 0 (- 201 x1p x2p x5p x10p x20p x50p) 100) 
+	x200p (range 0 (- 201 x1p x2p x5p x10p x20p x50p x100p) 200)
+	:when (= 200 (+ x1p x2p x5p x10p x20p x50p x100p x200p)) ] 
     (list x1p x2p x5p x10p x20p x50p x100p x200p)))
 
-;; limiting the ranges does not help at all
+;; limiting the ranges does not help at all (especially considering the error in the :when statement)
+;; problem031> (time (count (change)))
+;; "Elapsed time: 79883.61596 msecs"
+;; 73682
 
 (defn change []
-  (for [x1p   (range 0 201 1)   
-	x2p   (range 0 (+ (- 200 x1p) 1) 2)]
-    (for [x5p   (range 0 (+ (- 200 x1p x2p) 1) 5)	
-	  x10p  (range 0 (+ (- 200 x1p x2p x5p) 1) 10)
-	  x20p  (range 0 (+ (- 200 x1p x2p x5p x10p) 1) 20)
-	  x50p  (range 0 (+ (- 200 x1p x2p x5p x10p x20p) 1) 50)
-	  x100p (range 0 (+ (- 200 x1p x2p x5p x10p x20p x50p) 1) 100) 
-	  x200p (range 0 (+ (- 200 x1p x2p x5p x10p x20p x50p x100p) 1) 200)
-	  :when (= 200  (+ x1p x2p x1p x5p x10p x20p x50p x100p x200p)) ] 
-      (list x1p x2p x5p x10p x20p x50p x100p x200p))))
+  (for [x200p (range 0 201 200) 
+	x100p (range 0 (- 201 x200p) 100)
+	x50p  (range 0 (- 201 x200p x100p) 50)
+	x20p  (range 0 (- 201 x200p x100p x50p) 20)  
+	x10p  (range 0 (- 201 x200p x100p x50p x20p) 10)   
+	x5p   (range 0 (- 201 x200p x100p x50p x20p x10p) 5)	
+	x2p   (range 0 (- 201 x200p x100p x50p x20p x10p x5p) 2)
+	x1p   (range 0 (- 201 x200p x100p x50p x20p x10p x5p x2p) 1)   
+	:when (= 200  (+ x1p x2p x5p x10p x20p x50p x100p x200p)) ] 
+    (list x1p x2p x5p x10p x20p x50p x100p x200p)))
+
+;; flipping the ranges does help!
 
 ;; problem031> (time (count (change)))
-;; "Elapsed time: 61.178753 msecs"
-;; 10201
-
-(defn change []
-  (for [x1p   (range 0 201 1)     x2p   (range 0 201 2)]
-    (for [x5p   (range 0 201 5)	  x10p  (range 0 201 10)
-	  x20p  (range 0 201 20)  x50p  (range 0 201 50)
-	  x100p (range 0 201 100) x200p (range 0 201 200)
-	  :when (= 200  (+ x1p x2p x5p x10p x20p x50p x100p x200p)) ] 
-      (list x1p x2p x5p x10p x20p x50p x100p x200p))))
-;; problem031> (time (count (change)))
-;; "Elapsed time: 57.548397 msecs"
-;; 20301
-;; (* 201 101)
-;; the ranges are wrong
-
-;; Instead of checking which result is the correct one, we try to figure out another way to do this.
-;; aim for --> 39224 (problem031.pl, home to mama)
-
+;; "Elapsed time: 16083.125168 msecs"
+;; 73682
