21ad9cb84d0d8534c7ea60db800e29b15b90523f
Karsten Lang <karstenlang@gmail.com>
2010-05-08 19:32:02 +0200

    wrk files updated

--- project.el
+++ project.el
@@ -1,10 +1,12 @@
 (defwrk project-euler-current "problems currently being worked on"
   "~/projects/ProjectEuler/project.org"
   "~/projects/ProjectEuler/src/tools.clj"
-  "~/projects/ProjectEuler/src/problem078.clj"
-  "~/projects/ProjectEuler/src/problem031.clj"
+  "~/projects/ProjectEuler/src/tools/parts.clj"
   "~/projects/ProjectEuler/src/problem196.clj"
-  "~/projects/ProjectEuler/src/tools/parts.clj")
+  "~/projects/ProjectEuler/src/problem178.clj"
+  "~/projects/ProjectEuler/src/problem032.clj"
+  "~/projects/ProjectEuler/src/problem038.clj"
+)
 
 (defwrk project-euler-stuck "stuck problems"
   "~/projects/ProjectEuler/src/tools.clj"
@@ -12,13 +14,6 @@
   "~/projects/ProjectEuler/src/problem191.clj"
   ;"~/projects/ProjectEuler/src/problem282.clj"
   "~/projects/ProjectEuler/src/problem145.clj"
-  "~/projects/ProjectEuler/src/problem033.clj"
-  "~/projects/ProjectEuler/src/problem032.clj"
-  "~/projects/ProjectEuler/src/problem038.clj"
-  "~/projects/ProjectEuler/src/problem043.clj"
-  "~/projects/ProjectEuler/src/problem044.clj"
-  "~/projects/ProjectEuler/src/problem046.clj"
-  "~/projects/ProjectEuler/src/problem050.clj"
   "~/projects/ProjectEuler/src/problem051.clj"
   "~/projects/ProjectEuler/src/problem054.clj"
   "~/projects/ProjectEuler/src/problem060.clj"
@@ -30,7 +25,6 @@
   "~/projects/ProjectEuler/src/problem074.clj"
   "~/projects/ProjectEuler/src/problem075.clj"
   "~/projects/ProjectEuler/src/problem077.clj"
-  "~/projects/ProjectEuler/src/problem079.clj"
 )
 
 (defun project-euler nil
