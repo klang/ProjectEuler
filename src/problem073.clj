03fa6b00b2b6cf0f7d89fbadc013ec3c35dd52bd
Karsten Lang <karstenlang@gmail.com>
2010-05-12 19:15:09 +0200

    done - problem 73

--- src/problem073.clj
+++ src/problem073.clj
@@ -1,18 +1,19 @@
-;; Consider the fraction, n/d, where n and d are positive integers. If nd and HCF(n,d)=1, it is called a reduced proper fraction.
-;; 
-;; If we list the set of reduced proper fractions for d <  8 in ascending order of size, we get:
-;; 
-;; 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
-;; 
-;; It can be seen that there are 3 fractions between 1/3 and 1/2.
-;; 
-;; How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d <= 12,000?
-;; 
-;; Note: The upper limit has been changed recently.
-;; 
-(load "tools")
-(use 'clojure.contrib.math)
-
+(ns problem073
+  (meta {:description "Consider the fraction, n/d, where n and d are positive integers. If nd and HCF(n,d)=1, it is called a reduced proper fraction.
+ 
+If we list the set of reduced proper fractions for d <  8 in ascending order of size, we get:
+ 
+ 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+ 
+It can be seen that there are 3 fractions between 1/3 and 1/2.
+ 
+How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d <= 12,000?"})
+  (:use clojure.contrib.math)
+  (:use tools.numbers)
+  (:use tools.primes)
+  (:use problem072)
+  (:use clojure.test))
+ 
 (defn fractions [min max limit] 
   (sort (for [d (range 1 (+ limit 1)) 
 	      n (range 1 d) 
@@ -28,22 +29,6 @@
 ;; 50695
 ;; .. method not efficient for target limit (takes way more than a minute)
 
-; (+ 1/3 1/12000)
-
-(defn find-min [a b N]
-  "Implements the algorithm presented in 071_overview.pdf" 
-  (loop [bestNum 0
-	 bestDenom 1
-	 Ns (range 2 N)
-	 n 0]
-    (if (empty? Ns)
-      (list (/ bestNum bestDenom) n)
-      (let [currDenom (first Ns)
-	    currNum (quot (- (* a currDenom) 1) b)]
-	(if (< (* bestNum currDenom) (* currNum bestDenom))
-	  (recur currNum currDenom (rest Ns) (if (< 1/2 currNum) (inc n)))
-	  (recur bestNum bestDenom (rest Ns) (inc n))
-	  )))))
 
 ;; (sort (for [d (range 1 8) n (range 1 d) :when (= 1 (gcd n d))] (/ n d)))
 ;; (find-max 3 7 8)
@@ -51,3 +36,51 @@
 ;; user> (time (find-max 3 7 1000000))
 ;; "Elapsed time: 1867.093735 msecs"
 ;; 428570/999997
+
+;;-------------
+
+;; from problem 72, we have the number of reduced fractions in the entire range
+;; (time (- (F 12000) 2))
+;; "Elapsed time: 1010.601969 msecs"
+;; 43772257
+;; we also know, that 1/2 is always the middle element in farley series.
+
+(defn farey [n]
+  (map #(/ (first %) (second %)) 
+       (iterate 
+	(fn [[a b c d]] 
+	  (let [k (int (/ (+ n b) d))] 
+	    [c d (- (* k c) a) (- (* k d) b)])) [0 1 1 n])))
+
+(deftest test-fractions
+  (is
+   (= 3
+      (count (fractions 1/3 1/2 8))
+      (count (drop-while #(<= % 1/3)  (take-while #(< % 1/2) (farey 8))))))
+  (is
+   (= 505
+      (count (fractions 1/3 1/2 100))
+      (count (drop-while #(<= % 1/3)  (take-while #(< % 1/2) (farey 100))))))
+  (is
+   (= 50695
+      (count (fractions 1/3 1/2 1000))
+      (count (drop-while #(<= % 1/3)  (take-while #(< % 1/2) (farey 1000))))))
+  (comment
+    (is
+     (= 202766
+	(count (fractions 1/3 1/2 2000))
+	(count (drop-while #(<= % 1/3)  (take-while #(< % 1/2) (farey 2000))))))))
+
+;; to avoid realizing the entire sequence before being able to count it
+;; we wrap and count
+(defn count-fractions []
+  (loop [fractions (drop-while #(<= % 1/3) 
+			       (take-while #(< % 1/2) (farey 12000)))
+	 number 0]
+    (if (empty? fractions)
+      number
+      (recur (rest fractions) (inc number)))))
+;; (time (count-fractions))
+;; "Elapsed time: 364597.112721 msecs"
+;; 7295372
+
