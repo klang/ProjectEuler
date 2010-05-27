88698f82d7ce883bffb1a78bc87814025a385147
Karsten Lang <karstenlang@gmail.com>
2010-05-27 23:27:39 +0200

    problem 60 - stab

--- src/problem060.clj
+++ src/problem060.clj
@@ -10,6 +10,7 @@ Find the lowest sum for a set of five primes for which any two primes concatenat
   (:use problem038)
   (:use [clojure.contrib.lazy-seqs :only (primes)])
   (:use clojure.contrib.combinatorics)
+  (:use clojure.contrib.repl-utils)
   (:use clojure.set)
   (:use clojure.test))
 
@@ -30,8 +31,8 @@ Find the lowest sum for a set of five primes for which any two primes concatenat
   (is (working-set [3 7 109]))
   (is (working-set [3 7 109 673])))
 
-;; let's just work with limited primes
-(def limited-primes (primes-up-to 5000))
+;; let's just work with limited primes and not 2 or 5
+(def limited-primes (filter #(not (or (= 2 %) (= 5 %))) (primes-up-to 5000)))
 
 (comment
   (defn prime-concat? [[a b]]
@@ -43,12 +44,96 @@ Find the lowest sum for a set of five primes for which any two primes concatenat
   (filter prime-concat? (map #(list 7 %) limited-primes))
   )
 (defn p-concat [p] (into #{} (filter #(prime-concat? p %) limited-primes)))
+(defn p-concat-lazy [p] (filter #(prime-concat? p %) limited-primes))
 
-;; p7 #{673 229 2503 109 2707 1237 823 4729 541 4159}
-;(let [p3 (p-concat 3), p7 (p-concat 7)] (map  #(intersection p3 p7 (p-concat %)) p7))
+; (map #(list % (count (p-concat %))) (take 10 limited-primes))
 
-;(#{} #{} #{} #{109} #{673 229 2503 109 2707 1237 823 4729 541 4159} #{109} #{541} #{229 1237 823} #{} #{} #{1237} #{} #{} #{} #{} #{} #{} #{} #{823} #{229 4159} #{} #{541} #{1237} #{673} #{} #{} #{2707 1237} #{1237} #{} #{} #{} #{4159} #{} #{} #{} #{229} #{1237} #{} #{} #{4729} #{109} #{} #{} #{2707} #{109} #{109} #{} #{} #{109} #{} #{} #{} #{2503 1237 541} #{2707} #{1237} #{} #{} #{4159} #{} #{} #{} #{1237} #{541} #{} #{4159} #{} #{})
+(defn p-check [& primes]
+  "retuns the set of primes that can be concatenated with each prime in the primes given. The best effect is reached, if the primes given are members of each others p-maps. (p-check 3 7 109) will return the last member of the group, 673"
+  (reduce intersection (map #(p-concat %) primes)))
 
-;(let [pa (p-concat 3), pb (p-concat 11)] (map  #(intersection pa pb (p-concat %)) pb))
+(defn p-maps [prime-collection]
+  "for each prime in the collection, returns a set {p #{ primes that can be concatenated with p}}"
+  (loop [catch {}
+	 primes prime-collection]
+    (if (empty? primes)
+      catch
+      (recur (assoc catch (first primes)  (p-concat (first primes)))
+	     (rest primes)))))
 
-(map #(list % (count (p-concat %))) (take 10 limited-primes))
+(def p-maps-sequence (map #(hash-map % (p-concat %)) limited-primes))
+
+;; --------------------------------------------------------- depth first search
+
+;; queue: {3 [7 11 17 ..]}
+;; candidate: [3]
+;; add candidate element to queue if (every? true? (map  #(contains? (p-concat 7) %) [3]))
+;; ??? 
+;;; PROFIT
+;; queue: {3 [7 11 17 ..] 7 [3 19 61 ..]}
+;; find candidate element in (last candidate), if all memebers of 
+;; (every? true? (map  #(contains? (p-concat 109) %) [3 7]))
+
+;; (drop-while #(<= % (reduce max [3 7])) (sort (p-concat 109)) )
+
+(defn p-concat-lazy [p] (filter #(prime-concat? p %) limited-primes))
+
+(defn add-element [coll prime]
+  "an element is a prime and the lazily generated set of primes satisfying prime-concat?" 
+  (letfn [(p-concat [n] (filter #(prime-concat? p %) limited-primes))]
+    (assoc coll prime (p-concat prime))))
+
+(defn second-element-min
+  "given a collection of pairs, returns the pair that has the smallest second element. "
+  ; should deliver (7, 3) over (11, 3)
+  ([x] x)
+  ([x y] (if (and (not (or (nil? (second x)) (nil? (second y))))
+	      (<= (second x) (second y))) x y))
+  ([x y & more]
+   (reduce second-element-min (second-element-min x y) more)))
+
+(defn first-elements [coll]
+  (map #(list (first %) (first (second %))) coll))
+
+(defn next-element-index [coll]
+  "return the smallest element, that has the smallest first element. 7->3,.. 11->3,... returns (7,3)"
+  (reduce second-element-min (first-elements coll)))
+
+(def queue (sorted-map))
+(def queue (add-element queue 3))
+(def queue (add-element queue 7))
+(def queue (add-element queue 11))
+(def queue (add-element queue 19))
+;;(def queue (sorted-map 3 (p-concat-lazy 3) 7 (p-concat-lazy 7) 11 (p-concat-lazy 11) 19 (p-concat-lazy 19)))
+;;(def queue (hash-map 3 (p-concat-lazy 3) 11 (p-concat-lazy 11) 7 (p-concat-lazy 7)  19 (p-concat-lazy 19)))
+
+(defn depth-first-search [length]
+  (loop [queue (add-element (sorted-map) 3) ;; queue of elements to search
+	 dead []                            ;; deadends, every prime up to (first dead) and all dead are deadends
+	                                    ;; i.e. 109 can be a deadend, even if 11 hasn't been examined yet
+	 primes (rest limited-primes)       ;; it should be possible to use lazy sequence primes instead
+	 candidate [3]                       ;; 3,7,109,683 should be the first candidate of length 4
+	 limit 10                           ;;
+	 ]
+    (do (println {:first-elements (map #(list (first %) (first (second %))) queue)
+		  :next-element   (next-element-index queue)
+		  :first-dead     (first dead)
+		  :next-prime     (first primes)
+		  :candidate      candidate
+		  :iteration      limit
+		  }))
+    (if (or (= length (count candidate)) (zero? limit))
+      queue
+      (let [next-element (next-element-index queue)
+	    index (second next-element)]
+	
+	;; if index is in candidate, don't add the element to the queue, add the prime to the queue, drop the index and recur
+	;; 
+	(recur (if (nil? (queue index)) (add-element queue 7) queue) 
+	       dead 
+	       (if (= index (first primes)) (rest primes) primes)
+	       (conj candidate index)
+	       (dec limit)))
+      )))
+
+
