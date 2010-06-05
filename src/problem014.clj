2199c3f5e852d5be43adbbc16ca0765d50ca57f9
Karsten Lang <karstenlang@gmail.com>
2010-06-05 17:00:30 +0200

    namespace

--- src/problem014.clj
+++ src/problem014.clj
@@ -1,5 +1,5 @@
 (ns problem014
-  (meta) {:description "The following iterative sequence is defined for the set of positive integers:
+  (meta {:description "The following iterative sequence is defined for the set of positive integers:
 
 n -> n/2,     (n is even)
 n -> 3n + 1,  (n is odd)
@@ -10,7 +10,7 @@ Using the rule above and starting with 13, we generate the following sequence:
 It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 
 Which starting number, under one million, produces the longest chain?
-"})
+"}))
 
 (defn p014 [n]
   (loop [n n
