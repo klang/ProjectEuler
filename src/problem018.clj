7f646a7d67fb06751a2a1a11f0a9a9c9f914e246
Karsten Lang <karstenlang@gmail.com>
2010-06-05 17:17:03 +0200

    namespace

--- src/problem018.clj
+++ src/problem018.clj
@@ -1,18 +1,18 @@
-;; By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
-;; 
-;; 3
-;; 7 4
-;; 2 4 6
-;; 8 5 9 3
-;; 
-;; That is, 3 + 7 + 4 + 9 = 23.
-;; 
-;; Find the maximum total from top to bottom of the triangle below:
-;; 
-;; [snipped]
-;; 
-;; NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 (ns problem018
+  (meta {:description "By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+[snipped]
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o) "})
   (:use clojure.test)
   (:use [clojure.contrib.str-utils2 :only (split)]))
 
