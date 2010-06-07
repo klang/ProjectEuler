ea40bdb705f51bfda5199b5644c11987e2337ece
Karsten Lang <karstenlang@gmail.com>
2010-06-07 17:31:49 +0200

    done - problem 99

--- src/problem099.clj
+++ src/problem099.clj
@@ -1,40 +1,31 @@
 (ns problem099
   (meta {:description "Comparing two numbers written in index form like 2^11 and 3^7 is not difficult, as any calculator would confirm that 2^11 = 2048  3^7 = 2187.
 
-However, confirming that 632382^518061  519432^525806 would be much more difficult, as both numbers contain over three million digits."})
+However, confirming that 632382^518061 > 519432^525806 would be much more difficult, as both numbers contain over three million digits."})
   (:use tools.numbers)
   (:use tools.primes)
   (:use [clojure.contrib.str-utils2 :only (split)])
   (:use clojure.contrib.duck-streams)
+  (:use [clojure.contrib.generic.math-functions :only (log)] )
   (:use clojure.test))
 
 ;;(> (expt 632382 518061) (expt 519432 525806))
 ;; each number contains more than 3 million digits
 ;; don't even think about evaluating the term.
 
-;; maybe the factors can be used for something?
-;; user> (factors 632382)
-;; [2 3 105397]
-;; user> (factors 518061)
-;; [3 172687]
-;; 
-;; user> (factors 519432)
-;; [2 2 2 3 23 941]
-;; user> (factors 525806)
-;; [2 19 101 137]
-
 (def base-exp (slurp "base_exp.txt"))
-(map #(list
-       (factors (. Integer parseInt (nth % 0) 10)) 
-       (factors (. Integer parseInt (nth % 1) 10))) 
-     (take 5 (map #(split % #",") (split base-exp #"\r\n"))))
 
-(defn factor-base-exp [be]
+(defn lt [[base1 exp1] [base2 exp2]]
+  (< (* exp1 (log base1)) (* exp2 (log base2))))
+
+(deftest test-lt
+  (is (lt [519432 525806] [632382 518061]))
+  (is (lt [2 11] [3 7])))
+
+(defn string2integer [be]
   (let [base  (. Integer parseInt (nth be 0) 10)
-	exp   (. Integer parseInt (nth be 1) 10)
-	basef (factors base)
-	expf  (factors exp)]
-    (list (list base exp) basef expf)))
+	exp   (. Integer parseInt (nth be 1) 10)]
+    (list base exp)))
 
-(def factors-base-exp (map #(factor-base-exp %) 
-			   (map #(split % #",") (split base-exp #"\r\n"))))
+;;(last (sort lt (map #(string2integer %) (map #(split % #",") (split base-exp #"\r\n")))))
+;;(895447 504922) -> 709
