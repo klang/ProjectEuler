74af67e540acbf0de48c13876115aec86dffbe14
Karsten Lang <karstenlang@gmail.com>
2010-07-14 18:15:03 +0200

    problem 125 - some sums are missing

--- src/problem125.clj
+++ src/problem125.clj
@@ -10,8 +10,15 @@ Find the sum of all the numbers less than 10⁸ that are both palindromic and ca
 	tools.numbers
 	clojure.test))
 
-;; 1 4  9 16 25 36  49  64  81 100 121 144 169  196  225
-;; 1 5 14 30 55 91 140 204 285 385 506 650 819 1015 1240 
+;; 1² 2²  3²  4²  5²  6²  7²
+;; 1  4   9  16  25  36  49  64  81 100 121 144 169  196  225
+;; 1  5  14  30  55  91 140 204 285 385 506 650 819 1015 1240 
+;; 1² 1²+2² 1²+2²+3² 1²+2²+3²+4² 1²+2²+3²+4²+5² 1²+2²+3²+4²+5²+6² 1²+2²+3²+4²+5²+6²+7² ...
+;;             2²+3²    2²+3²+4²    2²+3²+4²+5²    2²+3²+4²+5²+6²    2²+3²+4²+5²+6²+7² ...
+;;                         3²+4²       3²+4²+5²       3²+4²+5²+6²       3²+4²+5²+6²+7² ...
+;;                                        4²+5²          4²+5²+6²          4²+5²+6²+7² ...
+;;                                                          5²+6²             5²+6²+7² ...
+;;                                                                               6²+7² ...
 
 (defn squared-sums []
   (drop 1 (map first (iterate (fn [[a b]] [(+ a (* b b)) (inc b)]) [0 1]))))
@@ -28,3 +35,40 @@ Find the sum of all the numbers less than 10⁸ that are both palindromic and ca
 ;; 1           2   3        4  5     6  7  8     9  10        11
 ;;((4 505 818) (9) (77 636) () (595) () () (181) () (121 434) (313) () ())
 ;; 
+
+(defn squared-sum-palindromes [n]
+  (filter #(< 0 %) 
+	  (filter palindrome? 
+		  (take-while #(< % 1000) (map #(- % n) (squared-sums))))))
+
+(defn squared-sums [starting]
+  (drop 2 (map first (iterate (fn [[a b]] [(+ a (* b b)) (inc b)]) [0 starting]))))
+
+(defn p125t []
+  (reduce + (filter palindrome? 
+		    (flatten (map 
+			      (fn [x] (take-while #(< % 1000) (squared-sums x)))
+			      (range 1 22))))))
+(deftest test-p125t
+  (is (= 4164 (p125t))))
+
+;; problem125> (time (p125t))
+;; "Elapsed time: 219.868231 msecs"
+;; 4164
+
+;; manual binary search for the last upper limit of the range
+;; (count (flatten (map (fn [x] (take-while #(< % (expt 10 8)) (squared-sums x))) [7070])))
+;; 1
+;; (count (flatten (map (fn [x] (take-while #(< % (expt 10 8)) (squared-sums x))) [7071])))
+;; 0
+
+(defn p125 []
+  (reduce + (filter palindrome? 
+		    (flatten (map 
+			      (fn [x] (take-while #(< % (expt 10 8)) (squared-sums x)))
+			      (range 1 7071))))))
+
+;; problem125> (time (p125))
+;; "Elapsed time: 7831.973044 msecs"
+;; 2916867073
+;; not correct
