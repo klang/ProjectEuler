401a342322119a68fcbb5863fe9257d075469f22
Karsten Lang <karstenlang@gmail.com>
2010-08-01 14:49:28 +0200

    Lyrics for 'Around the World' by Daft Punk

---
+++ src/daft.clj
@@ -0,0 +1,15 @@
+(ns daft
+  (meta {:description "The song 'Around the World' by Daft Punk has a deeply repetitive nature, unfortunately the lyrics is oftent stated wrong.
+The song has 4 verses consisting of 8, 16, 28 and 20 double repetitions of the phrase 'Around the world'."}))
+
+(def lyrics (repeat "Around the world, around the world"))
+
+(defn sing [lyrics]
+ (reduce str
+	 (interleave
+	  (map #(reduce str %)
+	       (map #(interleave % (repeat "\n"))
+		    (map #(take % lyrics) [8 16 28 20])))
+	  (repeat "\n") )))
+(comment
+  (println (sing song)))
