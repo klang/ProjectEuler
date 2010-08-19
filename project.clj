bbe6bf36e2c2172b119d45f5fc48c6e0ce9a1658
Karsten Lang <karstenlang@gmail.com>
2010-08-19 18:08:31 +0200

    bump to clojure 1.2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ProjectEuler "0.3"
   :description "Problems solved on http://projecteuler.net"
   :url "http://github.com/klang/ProjectEuler"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.3.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :jvm-opts ["-server" " -Xmx64M"])
 
